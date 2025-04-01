32b0788fe9046ad9371d594f8ecff6869214f049
Aleksander Długosz <olekdlugi@gmail.com>
2025-04-01 16:17:57 +0200

    Refactored fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,14 @@
-if type -q nvm
-    nvm use default --silent
-end
-
-if type -q minikube
-    eval $(minikube docker-env)
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set PATH $PATH $HOME/.bun/bin 
+    set PATH $PATH $HOME/.pub-cache/bin 
+    set PATH $PATH /opt/homebrew/bin /opt/homebrew/opt/llvm/bin
+    set PATH $PATH $HOME/Development/jextract-22/bin 
+
+    set DOCKER_HOST unix:///$HOME/.colima/default/docker.sock
+    set TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE /var/run/docker.sock
+end
+
+if type -q nvm
+    nvm use default --silent
 end
